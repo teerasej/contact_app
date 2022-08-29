e23a0c25bfb238c228a4cbf505358c2f54317aaa
teerasej <teerasej@gmail.com>
2022-08-29 13:09:34 +0700

    convert incoming data from api to list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:contact_app/models/contact_model.dart';
 import 'package:contact_app/pages/new_contact_page.dart';
 import 'package:contact_app/route_map.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -41,23 +42,44 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      // body: ListView.builder(
-      //   itemCount: _contacts.length,
-      //   itemBuilder: (BuildContext context, int index) {
-      //     var contact = _contacts[index];
-      //     return ListTile(
-      //       title: Text('${contact.firstName} ${contact.lastName}'),
-      //       subtitle: Text('${contact.phone}'),
-      //       // onTap: () {},
-      //       leading: InkWell(
-      //         child: Icon(Icons.person),
-      //         onTap: () {
-      //           print('tap');
-      //         },
-      //       ),
-      //     );
-      //   },
-      // ),
+      body: FutureBuilder(
+        future: Dio().get('https://randomuser.me/api/?results=100'),
+        builder:
+            (BuildContext context, AsyncSnapshot<Response<dynamic>> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('${snapshot.error}'),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            var json = snapshot.data!.data;
+            var listOfUsers = json['results'] as List;
+            _contacts = listOfUsers.map<ContactModel>(
+              (e) {
+                return ContactModel.fromMap(e);
+              },
+            ).toList();
+            return ListView.builder(
+              itemCount: _contacts.length,
+              itemBuilder: (BuildContext context, int index) {
+                var contact = _contacts[index];
+                return ListTile(
+                  title: Text('${contact.firstName} ${contact.lastName}'),
+                  subtitle: Text('${contact.phone}'),
+                  // onTap: () {},
+                  leading: InkWell(
+                    child: Icon(Icons.person),
+                    onTap: () {
+                      print('tap');
+                    },
+                  ),
+                );
+              },
+            );
+          }
+
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
