a19895be716e5cf484062cefe3bc7a5f158a50d6
teerasej <teerasej@gmail.com>
2022-08-29 13:09:38 +0700

    convert data

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -1,9 +1,15 @@
 // lib/models/contact_model.dart
 
 class ContactModel {
-  String firstName;
-  String lastName;
-  String phone;
+  late String firstName;
+  late String lastName;
+  late String phone;
 
   ContactModel({this.firstName = '', this.lastName = '', this.phone = ''});
+
+  ContactModel.fromMap(Map jsonMap) {
+    firstName = jsonMap['name']['first'];
+    lastName = jsonMap['name']['last'];
+    phone = jsonMap['phone'];
+  }
 }
